94329c38c39090468bd6636a084cc8de25889dce
Gasper Martic <martic.gasper@gmail.com>
2019-10-24 13:40:03 +0200

    add files

---
+++ Models/Note.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Z01.Models
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string[] CategoriesList { get; set; }
+        public DateTime Date { get; set; }
+        public FileType FileType { get; set; }
+    }
+
+    public static class Utilities {
+        public static Note[] GetNotes() {
+            Note[] allNotes = new Note[1]; // change 1 to n, which you get from counting files in root/notes
+            
+            Note test = new Note();
+            test.Title="TestTitle";
+            test.Content="Test content 123 abcd";
+            test.CategoriesList=new string[2] {"sport","random"};
+            test.Date=DateTime.Now;
+            allNotes[0] = test;
+            
+            return allNotes;
+        }
+    }
+
+    public enum FileType {
+        Txt = 0,
+        Md = 1
+    }
+}
